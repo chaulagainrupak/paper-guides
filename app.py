e3b6bb01779784f93f1b68c104d008b5da5a0496
Rupak Chaulagain <rupak.chaulagainO19@gmail.com>
2024-11-19 23:02:58 +0545

    hotfix

--- app.py
+++ app.py
@@ -47,11 +47,8 @@ login_manager.init_app(app)
 
 # Create the database 
 createDatabase()
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
+with app.app_context():
+    db.create_all()
 
 @login_manager.user_loader
 def load_user(user_id):
