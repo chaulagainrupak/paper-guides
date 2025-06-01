0b13b989e65193b82a1269f741df4dfde9ef64fe
chaulagainrupak <rupak.chaulagainO19@gmail.com>
2025-06-01 11:09:24 +0545

    vercel deployment

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,4 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +13,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: vercel()
 	}
 };
 
