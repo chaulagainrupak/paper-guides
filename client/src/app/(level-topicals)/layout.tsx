99a8cd015a6a8ed73a0deaceb37fdf43000c9caa
chaulagainrupak <rupak.chaulagainO19@gmail.com>
2025-06-03 11:14:12 +0545

    sidebar fix

--- client/src/app/(level-topicals)/layout.tsx
+++ client/src/app/(level-topicals)/layout.tsx
@@ -1,9 +1,7 @@
 export default function LevelsLayout({
   children,
-  sidebar,
 }: {
   children: React.ReactNode;
-  sidebar: React.ReactNode;
 }) {
   return (
     <div className="mt-[64] h-screen px-6 py-6 flex xl:flex-row flex-col gap-6">
@@ -11,13 +9,9 @@ export default function LevelsLayout({
         <main>{children}</main>
       </div>
     
-
-    {
-      sidebar ?? (
       <div className="promo-space bg-[var(--baby-powder)] xl:h-full h-1/4 xl:w-1/4 shadow-xl rounded-md p-6">
             ADVERT
-      </div>)
-    }
+      </div>
     </div>
   );
 }
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
 import ThemeToggleButton from "./themeToggle";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Paper Guides",
