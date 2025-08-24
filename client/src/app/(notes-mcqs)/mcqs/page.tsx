58e130c84fd1f8103f39d963b51b5c56ad38d6f3
chaulagainrupak <rupak.chaulagainO19@gmail.com>
2025-08-24 17:56:30 +0545

    OPG fix

--- client/src/app/(notes-mcqs)/mcqs/page.tsx
+++ client/src/app/(notes-mcqs)/mcqs/page.tsx
@@ -2,19 +2,19 @@ import { Metadata } from "next";
 import QuestionGeneratorForm from "./QuestionGeneratorForm.jsx";
 
 export const metadata: Metadata = {
-  title: "Paper Guides | Past Papers ",
+  title: "Paper Guides | MCQs Generator ",
   description:
     "Free exam preparation resources, Notes, Past-Papers, Question Papers, Question Generator, MCQs practice and more.  Find past papers for O-Level, A-Levels, NEB / TU / IOE and more.",
   icons: "/images/logo.ico",
   openGraph: {
-    title: "Paper Guides | Past Papers",
+    title: "Paper Guides | MCQs Generator",
     description:
       "Free exam preparation resources, Notes, Past-Papers, Question Papers, Question Generator, MCQs practice and more. Find past papers for O-Level, A-Levels, NEB / TU / IOEand more.",
-    url: "https://paperguides.org",
+    url: "https://paperguides.org/mcqs",
     siteName: "Paper Guides",
     images: [
       {
-        url: "/images/opg_mcqs_question_generator.png",
+        url: "/images/opg_mcqs_enerator.png",
         width: 1200,
         height: 720,
         alt: "Paper Guides Open Graph Image",
--- client/src/app/generator/page.tsx
+++ client/src/app/generator/page.tsx
@@ -3,15 +3,16 @@ import { Metadata } from "next";
 import QuestionGeneratorForm from "./QuestionGeneratorForm.jsx";
 
 export const metadata: Metadata = {
-  title: "Paper Guides | Past Papers ",
+  title: "Paper Guides | Question Generator ",
   description:
     "Free exam preparation resources, Notes, Past-Papers, Question Papers, Question Generator, MCQs practice and more.  Find past papers for O-Level, A-Levels, NEB / TU / IOE and more.",
   icons: "/images/logo.ico",
   openGraph: {
-    title: "Paper Guides | Past Papers",
+    title: "Paper Guides | Question Generator",
+
     description:
       "Free exam preparation resources, Notes, Past-Papers, Question Papers, Question Generator, MCQs practice and more. Find past papers for O-Level, A-Levels, NEB / TU / IOEand more.",
-    url: "https://paperguides.org",
+    url: "https://paperguides.org/generator",
     siteName: "Paper Guides",
     images: [
       {
