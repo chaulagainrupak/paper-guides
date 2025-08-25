0d0c5f8c3520e953cb267780f8b9f1f734b71b84
Rupak Chaulagain <86196413+chaulagainrupak@users.noreply.github.com>
2025-08-25 14:06:57 +0545

    Update layout.tsx

--- client/src/app/(level-topicals)/layout.tsx
+++ client/src/app/(level-topicals)/layout.tsx
@@ -1,27 +1,3 @@
-import { Metadata } from "next";
-export const metadata: Metadata = {
-  title: "Paper Guides | Past Papers ",
-  description:
-    "Free exam preparation resources, Notes, Past-Papers, Question Papers, and more.  Find past papers for O-Level, A-Levels, NEB / TU / IOE and more.",
-  icons: "/images/logo.ico",
-  openGraph: {
-    title: "Paper Guides | Past Papers",
-    description:
-      "Free exam preparation resources, Notes, Past-Papers, Question Papers, and more. Find past papers for O-Level, A-Levels, NEB / TU / IOEand more.",
-    url: "https://paperguides.org/topicals",
-    siteName: "Paper Guides",
-    images: [
-      {
-        url: "/images/topicals.png",
-        width: 1200,
-        height: 720,
-        alt: "Paper Guides Open Graph Image",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-};
 
 export default function LevelsLayout({
   children,
