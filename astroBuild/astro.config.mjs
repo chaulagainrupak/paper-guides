f826b488f2702108b874ec9f4671cf7dd69c8d65
chaulagainrupak <rupak.chaulagainO19@gmail.com>
2026-03-10 18:45:08 +0545

    build

--- astroBuild/astro.config.mjs
+++ astroBuild/astro.config.mjs
@@ -1,19 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from "@astrojs/cloudflare";
 
 import react from "@astrojs/react";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  adapter: cloudflare(),
+  output: "server",
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+    adapter: node({
+    mode: "standalone"
+  })
 });
